e574c51b0b45d5456644a4aecfc0c34bec60548f
EJ <eric_j_fra@yahoo.fr>
2023-08-17 15:12:40 +0200

    12- fix cards display after close tag(s)

--- scripts/utils/filter.js
+++ scripts/utils/filter.js
@@ -62,6 +62,7 @@ export const filterFunction = () => {
   cssByNumberOfCard()
   filterSelectList(result)
   noFiltermatch(result, text)
+  cardsWrapper.classList.remove('cards_wrapper_filter_no-match') //TODO when tag(s) remove without no cards display
 }
 
 /**
