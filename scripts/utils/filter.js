37f6895bc956ca87ecef8bbf624adbe835b9ed2a
EJ <eric_j_fra@yahoo.fr>
2023-08-18 13:33:47 +0200

    minor change

--- scripts/utils/filter.js
+++ scripts/utils/filter.js
@@ -62,7 +62,6 @@ export const filterFunction = () => {
   cssByNumberOfCard()
   filterSelectList(result)
   noFiltermatch(result, text)
-  cardsWrapper.classList.remove('cards_wrapper_filter_no-match') //TODO when tag(s) remove without no cards display
 }
 
 /**
